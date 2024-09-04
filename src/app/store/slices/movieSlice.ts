142ab3ea52f14088dbf11f1b3f999fd89020eed8
Leo Shih <leoshih@qnap.com>
2024-09-04 22:53:07 +0800

    Remove unused async thunk

--- src/app/store/slices/movieSlice.ts
+++ src/app/store/slices/movieSlice.ts
@@ -1,28 +1,13 @@
-import { createSlice, createAsyncThunk, PayloadAction } from '@reduxjs/toolkit';
-import { AppDispatch } from 'store/index';
-import {
-  search
-} from 'services/Movies';
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
 type movieState = {
   movies: FormattedResponseMovie[]|null,
+  totalResults: number,
 }
-
-export const searchMovies = createAsyncThunk<
-  FormattedTmdbResponse<FormattedResponseMovie>,
-  string,
-  { dispatch: AppDispatch }
->(
-  "movie/searchMovies",
-  async (keyword:string, { dispatch } ) => {
-    dispatch(mutateSearchResult({ movies: null, totalResults: 0 }))
-
-    return search(keyword)
-  }
-)
 
 const initialState: movieState = {
   movies: null,
+  totalResults: 0,
 }
 
 const movieSlice = createSlice({
@@ -30,18 +15,10 @@ const movieSlice = createSlice({
   initialState,
   reducers: {
     mutateSearchResult(state, action:PayloadAction<{ movies: FormattedResponseMovie[]|null, totalResults:number }>) {
-      console.log('action.payload.movies :>> ', action.payload.movies);
       state.movies = action.payload.movies
+      state.totalResults = action.payload.totalResults
     },
   },
-  extraReducers: (builder) => {
-    builder
-      .addCase(searchMovies.fulfilled, (state, action: PayloadAction<{ movies: FormattedResponseMovie[]|null, totalResults:number }>) => {
-        const { movies } = action.payload
-
-        state.movies = movies
-      })
-  }
 })
 
 export const { 
